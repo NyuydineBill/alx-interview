d32cea48d84317abcd51e24379f2e802131a6928
NyuydineBill <billleynyuy@gmail.com>
2024-08-04 19:59:57 +0100

    Done with the task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Solution to Lockboxes problem"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
