490dbc87231a92c8cdd4a1b55c6c8bda5e79468d
NyuydineBill <billleynyuy@gmail.com>
2024-08-04 20:28:01 +0100

    Done with the task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
+"""Solution to Lockboxes problem"""
+
 
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes can be unlocked.
-
-    Parameters:
-    boxes (list of lists): A list where each element is a list containing keys to other boxes.
-
-    Returns:
-    bool: True if all boxes can be unlocked, otherwise False.
-    """
-    n = len(boxes)  # Number of boxes
-    unlocked = [False] * n  # Boolean list to track which boxes are unlocked
-    unlocked[0] = True  # The first box is always unlocked
-    keys = [0]  # Start with the first box's keys
-    
-    while keys:
-        current_key = keys.pop()  # Get the next key to use
-        for key in boxes[current_key]:  # Iterate over all keys in the current box
-            if key < n and not unlocked[key]:  # Check if the key is valid and the box is not yet unlocked
-                unlocked[key] = True  # Mark the box as unlocked
-                keys.append(key)  # Add the key to the list of keys to use
-    
-    return all(unlocked)  # Check if all boxes have been unlocked
+    """Determines if all the boxes can be opened"""
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
