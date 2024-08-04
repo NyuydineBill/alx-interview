86d29b34a4195a72e445da2b6357b204497e5138
NyuydineBill <billleynyuy@gmail.com>
2024-08-04 20:10:00 +0100

    Done with the task

--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
 
-def canUnlockAll(boxes):
-    n = len(boxes)
-    unlocked = [False] * n
-    unlocked[0] = True  # The first box is always unlocked
-    keys = [0]  # Start with the first box
-    
-    while keys:
-        current_key = keys.pop()
-        for key in boxes[current_key]:
-            if key < n and not unlocked[key]:
-                unlocked[key] = True
-                keys.append(key)
-    
-    return all(unlocked)
+canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-# Test cases
 boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))  # Expected output: True
+print(canUnlockAll(boxes))
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))  # Expected output: True
+print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
