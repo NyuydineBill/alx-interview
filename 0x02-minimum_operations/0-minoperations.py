d00d3d35e3dad01904733f7e83436963e3dd9a36
NyuydineBill <billleynyuy@gmail.com>
2024-08-11 22:02:35 +0100

    Done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-
-
 def minOperations(n):
     if n <= 1:
         return 0
-    
     operations = 0
     factor = 2
-    
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
     return operations
