f8e11884eb716aa277373b00d6431fd7e293430d
NyuydineBill <billleynyuy@gmail.com>
2024-09-30 00:11:23 +0100

    Updated

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,54 +1,25 @@
 #!/usr/bin/python3
-"""Island Perimeter - ALX Interview"""
-
-
-def check_val(x):
-    """_summary_
-
-    Args:
-        n (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    if (x == 0):
-        return 1
-    return 0
+"""Island perimeter computing module.
+"""
 
 
 def island_perimeter(grid):
-    """_summary_
-
-    Args:
-        grid (_type_): _description_
+    """Computes the perimeter of an island with no lakes.
     """
-
-    row = len(grid)
-    col = len(grid[0])
-    assert (1 <= row and col <= 100), "length must be between 1 an 100"
-
-    x = 0
-    for i in range(row):
-        for j in range(col):
-            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
-                "grid numbers must be 0 or 1"
-            if grid[i][j] == 1:
-                if i-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i-1][j])
-                if j-1 < 0:
-                    x += 1
-                else:
-                    x += check_val(grid[i][j-1])
-
-                try:
-                    x += check_val(grid[i+1][j])
-                except IndexError:
-                    x += 1
-                try:
-                    x += check_val(grid[i][j+1])
-                except IndexError:
-                    x += 1
-
-    return x
+    perimeter = 0
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
+    return perimeter
